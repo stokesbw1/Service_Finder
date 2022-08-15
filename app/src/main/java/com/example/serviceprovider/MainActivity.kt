bc45bc0fe3c4e5bc814221f287efb3d65f4ccb29
innocent <innocent@pcgsoftware.co>
2022-08-15 17:43:34 +0200

    setting up view pager

--- app/src/main/java/com/example/serviceprovider/MainActivity.kt
+++ app/src/main/java/com/example/serviceprovider/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.serviceprovider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_main.*
+import me.relex.circleindicator.CircleIndicator3
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+fun setUpViewPager(){
+      val titlesList = ArrayList<String>(listOf(*resources.getStringArray(R.array.FIRST_TITLE_LIST)))
+      val descList = ArrayList<String>(listOf(*resources.getStringArray(R.array.SPLASH_DESC)))
+
+    view_pager2.adapter=ViewPagerAdapter(titlesList, descList, listOf(R.mipmap.ic_launcher_round,R.mipmap.ic_launcher_round,R.mipmap.ic_launcher_round))
+    view_pager2.orientation =ViewPager2.ORIENTATION_HORIZONTAL
+
+    var  indicator:CircleIndicator3 = findViewById<CircleIndicator3>(R.id.indicator)
+    indicator.setViewPager(view_pager2)
+}
 }
